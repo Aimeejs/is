a400f8b4174481726cab48ee29fd92d96ec36f32
gavinning <ningyubo@gmail.com>
2015-07-08 14:08:17 +0800

    优化

--- is.js
+++ is.js
@@ -11,36 +11,36 @@ var toString = Object.prototype.toString;
 /**
  * [数组类型判定]
  * @param  {[object]}  obj  [要检查的对象]
- * @return {[boolean]}     	[true or false]
+ * @return {[boolean]}      [true or false]
  */
 is.array = function (obj) {
-	if (typeof Array.isArray === 'function') {
-		return Array.isArray(obj);
-	}
+    if (typeof Array.isArray === 'function') {
+        return Array.isArray(obj);
+    }
 
-	return toString.call(obj) === '[object Array]';
+    return toString.call(obj) === '[object Array]';
 }
 
 is.plainObject = function isPlainObject(obj) {
-	'use strict';
+    'use strict';
 
-	if (!obj || toString.call(obj) !== '[object Object]') {
-		return false;
-	}
+    if (!obj || toString.call(obj) !== '[object Object]') {
+        return false;
+    }
 
-	var hasOwnConstructor = hasOwn.call(obj, 'constructor');
-	var hasIsPrototypeOf = obj.constructor && obj.constructor.prototype && hasOwn.call(obj.constructor.prototype, 'isPrototypeOf');
-	// Not own constructor property must be Object
-	if (obj.constructor && !hasOwnConstructor && !hasIsPrototypeOf) {
-		return false;
-	}
+    var hasOwnConstructor = hasOwn.call(obj, 'constructor');
+    var hasIsPrototypeOf = obj.constructor && obj.constructor.prototype && hasOwn.call(obj.constructor.prototype, 'isPrototypeOf');
+    // Not own constructor property must be Object
+    if (obj.constructor && !hasOwnConstructor && !hasIsPrototypeOf) {
+        return false;
+    }
 
-	// Own properties are enumerated firstly, so to speed up,
-	// if last one is own, then all properties are own.
-	var key;
-	for (key in obj) {/**/}
+    // Own properties are enumerated firstly, so to speed up,
+    // if last one is own, then all properties are own.
+    var key;
+    for (key in obj) {/**/}
 
-	return typeof key === 'undefined' || hasOwn.call(obj, key);
+    return typeof key === 'undefined' || hasOwn.call(obj, key);
 }
 
 is.string = function(obj){
